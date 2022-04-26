3112efe6d1e9fc8545a73f7774aa5e5d7ed4d3ce
ulyana474 <uad474@gmail.com>
2022-04-25 18:53:01 -0700

    opens google in new tab

---
+++ src/RepoDetails.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import RepoItem from "./RepoItem";
+
+const RepoDetails = (props) => {
+    return (
+      <div>
+        You choose {props.name}
+      </div>
+    )
+  }
+
+export default RepoDetails;
--- src/RepoItem.js
+++ src/RepoItem.js
@@ -1,11 +1,22 @@
 import React from 'react';
-	
-const RepoItem = (props) => {
+
+class RepoItem extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  openInNewTab = url => {
+    window.open(url, '_blank', 'noopener,noreferrer');
+  };
+  onClick = (event) =>{
+    alert("click")
+  }
+  render(){
   return (
-    <div>
-      {props.item}
+    <div onClick={() => this.openInNewTab('https://google.com')}>
+      {this.props.item}
     </div>
   )
+}
 }
 
 export default RepoItem;
